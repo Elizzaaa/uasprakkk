17f51d6b8292feabd30017d2be2a779d464f0cb3
candra nababan <candranababan95@gmail.com>
2024-06-25 13:09:57 +0700

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div class="q-pa-md">
+        <WeatherWidget />
+    </div>
+  </template>
+
+<script>
+import WeatherWidget from '../components/WeatherWidget.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    WeatherWidget
+  }
+};
+</script>
+  
